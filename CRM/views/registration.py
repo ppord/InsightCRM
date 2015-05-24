f1e08f0d17d7cec1620ad2e4bd860a8627d50fe9
dropp <droppmail@gmail.com>
2015-05-24 17:48:10 +0300

    Registration: templates

---
+++ CRM/views/registration.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from ..models import Invite
+import random
+import string
+from django.core.mail import send_mail
+
+
+
+def send_register_mail(request):
+    if (request.GET['email'] is not None) and (request.GET['email'] != ''):
+        invite = Invite()
+        invite.email = request.GET['email']
+        randomsalt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+        invite.invite = '%s%s' % (request.GET['email'], randomsalt)
+        invite.valid = True
+        invite.clean()
+        invite.save()
+        print(invite.invite)
+        send_mail('InsightCMS', 'http://testcloud.ru/register/?invite=%s' % invite.invite,
+                  'robot@testcloud.ru', [request.GET['email']])
+        print('sended')
+        return render_to_response('info.html',
+                                  {'infomsg': 'Email sended.'},
+                                  context_instance = RequestContext(request))
+
+    else:
+        print('error')
+        return HttpResponse('Something wrong')
+
+
+def indexpage(request):
+    return render_to_response('index.html', context_instance=RequestContext(request))
