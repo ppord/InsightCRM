fde8a63983a7d4a555142b28b34fa53b564eed37
dropp <droppmail@gmail.com>
2015-05-26 17:41:28 +0300

    Assets moved to assets

--- CRM/views/registration.py
+++ CRM/views/registration.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from ..models import Invite
